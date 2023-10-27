f3dc8cb8214547b9a924299da08433d2224e70aa
Alvaro06 <qnp58011@educastur.es>
2023-10-27 16:24:33 +0200

    27/10/2023

--- src/ADT/Clases/LeerXMLDOM.java
+++ src/ADT/Clases/LeerXMLDOM.java
@@ -3,29 +3,28 @@ package ADT.Clases;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import sun.plugin.dom.core.Document;
+import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Objects;
 
 /*
 Objetivo de cargar y leer el fichero xml
  */
 public class LeerXMLDOM {
 
-    ArrayList<Empleado> f = new ArrayList<>();
+    ArrayList<Empleado> lista = new ArrayList<>();
 
     public void LeerXml() {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = (Document) builder.parse(new File("Empleados.xml"));  // El metodo parse cooge el fichero y lo mete en una estructura de listas
+            Document document =  builder.parse(new File("file/Empleados.xml"));  // El metodo parse cooge el fichero y lo mete en una estructura de listas
             document.getDocumentElement().normalize();
             /*
-            A partir de aqui extraemos la
+            A partir de aqui extraemos la informacion
              */
             System.out.printf("Elemento raiz: %s %n", document.getDocumentElement().getNodeName());
             //crea lista con todos los nodos empleados
@@ -36,19 +35,31 @@ public class LeerXMLDOM {
                 Node emple = empleados.item(i); // obtener un nodo empleado  / item(i) equivale al get del ArrayList
                 if (emple.getNodeType() == Node.ELEMENT_NODE) {
                     Element elemento = (Element) emple;
-                    String id = elemento.getElementsByTagName("id").item(0).getTextContent();
+                    int id = Integer.parseInt(elemento.getElementsByTagName("id").item(0).getTextContent());
                     String apellido = elemento.getElementsByTagName("apellido").item(0).getTextContent();
-                    int dep = Integer.parseInt(elemento.getElementsByTagName("dep").item(0).getTextContent());
-                    float salario = Float.parseFloat(elemento.getElementsByTagName("salario").item(0).getTextContent());
+                    int dep = Integer.parseInt((elemento.getElementsByTagName("dep").item(0).getTextContent()));
+                    float salario = Float.parseFloat((elemento.getElementsByTagName("salario").item(0).getTextContent()));
+                    Empleado e = new Empleado(salario,id,apellido,dep);
+                    lista.add(e);
+
                 }
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+    public void mostrarLista(){
+        StringBuilder empleados = new StringBuilder();
+        for (Empleado l:lista) {
+            empleados.append(l.toString());
+        }
+        System.out.println(empleados);
+    }
 
     public static void main(String[] args) {
-
+        LeerXMLDOM l = new LeerXMLDOM();
+        l.LeerXml();
+        l.mostrarLista();
     }
 }
 
